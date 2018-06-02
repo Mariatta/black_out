80e628f642a5121977a7d74ae0817f12ac573cbd
Mariatta Wijaya <mariatta@python.org>
2018-06-01 20:26:30 -0700

    Clone the repo into `repo_checkout` directory.

--- black_out/util.py
+++ black_out/util.py
@@ -31,12 +31,12 @@ def check_black(src):
 def commit_changes(issue_number):
     title = "🤖 Format code using `black`"
     body = f"""
-
-Closes #{issue_number}.
-(I'm a bot 🤖)
+    
+    Closes #{issue_number}.
+    (I'm a bot 🤖)
     """
     message = title + body
-    cmd = ["git", "commit", "--am", message]
+    cmd = ["git", "commit", "-am", message]
     subprocess.check_output(cmd)
     return title, body
 
